c52aa076e27031f31f9df79bc7c5dedf0c939a8f
Neha Girja <nehagirja09@gmail.com>
2025-07-23 14:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (29.34%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,3 +3,8 @@ class Solution:
         unique = sorted(set(nums))
         nums[:len(unique)] = unique
         return len(unique)
+
+# Set insertion takes O(1) time complexity
+# Sorted - O(nlogn)
+# Slicing - O(n)
+# Sorting dominated - Hence, O( nlogn)
