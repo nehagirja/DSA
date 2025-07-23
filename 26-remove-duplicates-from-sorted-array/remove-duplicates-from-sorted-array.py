3e7f4a4ab918e2141c746bb503ccd63ecdeb27ae
Neha Girja <nehagirja09@gmail.com>
2025-07-23 16:33:21 +0530

    Time: 3 ms (47.00%) | Memory: 19.1 MB (29.34%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,10 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        unique = sorted(set(nums))
-        nums[:len(unique)] = unique
-        return len(unique)
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[l - 1]:
+                nums[l] = nums[r]
+                l += 1
+        return l
 
-# Set insertion takes O(1) time complexity
-# Sorted - O(nlogn)
-# Slicing - O(n)
-# Sorting dominated - Hence, O( nlogn)
+
+# l is the placeholder - first replace, then increment 
+# r compares only 
+# TC O(n)
