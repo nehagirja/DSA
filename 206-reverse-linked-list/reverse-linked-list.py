2fea4c4c577a8f3603d137ba1dd90146b70cce79
Neha Girja <nehagirja09@gmail.com>
2026-01-20 13:27:01 -0500

    Time: 0 ms (100.00%) | Memory: 20.7 MB (7.72%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,14 +6,13 @@
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-       prev, curr = None, head
-    #    time complxity O(n) space O(1)
-       while curr:
+         prev, curr = None, head
+         while curr:
             nxt = curr.next
             curr.next = prev
             prev = curr
-            curr = nxt
-       return prev
+            curr = nxt 
+         return prev
 
 # **iterative solution**
 # store the next of the current in "nxt" variable so that it's location does not get lost when we point the current's next to the previous to make it a tail because prev is actually Null 
