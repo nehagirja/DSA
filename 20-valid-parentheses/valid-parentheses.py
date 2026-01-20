987d2ea185c1b7e33f9a781b28a786cdb4e681c7
Neha Girja <nehagirja09@gmail.com>
2026-01-19 20:48:16 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (22.51%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,25 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []  
-        # Stack represents "pending" opening brackets that need to be closed
-
-        matchingSet = {
-            ")" : "(",
-            "}" : "{",
-            "]" : "["
-        }
-        for c in s:
-            if c in matchingSet:
-                if stack and stack[-1] == matchingSet[c]:
+       stack = []
+       closeToOpen = { ")" : "(" , "]" : "[", "}" : "{" }
+       for c in s:
+            if c in closeToOpen:
+                if stack and closeToOpen[c] == stack[-1]:
                     stack.pop()
-                else:
-                   return False 
-                #if opening bracket is not encountered first but instead a closing bracket is
+                else: 
+                    return False
             else:
-                stack.append(c) 
-                # opening brackets get puched because of this 
-        return True if not stack else False 
-        # if stack is empty after iteration else false 
+                stack.append(c)
+       return True if not stack else False  # covering edge case of bracket appearing without the closing bracket like {([])
+
+
+        
+
+
+            
         
             
 
