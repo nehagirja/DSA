5725dd4d22ff3246b516ff93f6f88f6ca75c105a
Neha Girja <nehagirja09@gmail.com>
2026-02-18 18:58:50 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (54.29%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -24,3 +24,5 @@ class Solution:
                     queue.append(node.right)
             output.append(level[-1])
         return output 
+
+# The space complexity of this BFS level order traversal is O(n) because in the worst case the queue can store up to half of the nodes in the tree at once (such as the last level of a complete binary tree), which is proportional to n. Although the level list is recreated for each level, at any moment it can hold up to the number of nodes in the largest level, which is also O(n) in the worst case. The output list stores one value per level, which could be O(n) in a skewed tree. Since we consider the maximum space used at any time, the overall space complexity is O(n).
