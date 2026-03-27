67799f8224837c5bfe65809fb8c73c2da4a322a9
Neha Girja <nehagirja09@gmail.com>
2026-03-26 21:25:42 -0400

    Time: 3 ms (98.93%) | Memory: 19.5 MB (87.73%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,27 +3,24 @@ class Solution:
         if s == " ":
             return True
 
-        string = s.lower() # O(n)
-
         left, right = 0, len(s)-1
 
         # O(n)
         while left < right:
-            if not string[left].isalnum():
+            if not s[left].isalnum():
                 left += 1
                 continue
-            if not string[right].isalnum():
+            if not s[right].isalnum():
                 right -= 1
                 continue
-            if string[left] == string[right]:
-                left += 1
-                right -= 1
-            else:
+            if s[left].lower() != s[right].lower():
                 return False
+            left, right = left + 1, right - 1
         return True
 
+
 # total time complexity = O(n)
-#  and space is O(n)        
+#  and space is O(1)        
 
         
 
