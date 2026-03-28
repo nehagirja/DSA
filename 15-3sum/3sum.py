e41b7720844fecd5f33dd537cd4b35b7b3c81591
Neha Girja <nehagirja09@gmail.com>
2026-03-28 16:54:44 -0400

    Time: 643 ms (51.72%) | Memory: 22.2 MB (87.98%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -20,8 +20,8 @@ class Solution:
                     r -= 1
                     while l < r and nums[l] == nums[l-1]:
                         l += 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
+                    # while l < r and nums[r] == nums[r + 1]:
+                    #     r -= 1
         return res
                 
             
